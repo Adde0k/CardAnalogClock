e90a4aeb9c633f0d11dfc97b726328d2567781f6
Luca A <lucale1114@skola.goteborg.se>
2022-09-20 12:48:33 +0200

    Added tz and digital

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from 'src/material-module';
 
 import { AppComponent } from './app.component';
 import { CardAnalogClockComponent } from './card-analog-clock/card-analog-clock.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CardAnalogClockComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CardAnalogClockComponent,
+    ],
+    imports: [
+        BrowserModule,
+        MaterialModule,
+        BrowserAnimationsModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/app/card-analog-clock/card-analog-clock.component.spec.ts
+++ src/app/card-analog-clock/card-analog-clock.component.spec.ts
@@ -3,21 +3,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CardAnalogClockComponent } from './card-analog-clock.component';
 
 describe('CardAnalogClockComponent', () => {
-  let component: CardAnalogClockComponent;
-  let fixture: ComponentFixture<CardAnalogClockComponent>;
+    let component: CardAnalogClockComponent;
+    let fixture: ComponentFixture<CardAnalogClockComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CardAnalogClockComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [CardAnalogClockComponent],
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(CardAnalogClockComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(CardAnalogClockComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
